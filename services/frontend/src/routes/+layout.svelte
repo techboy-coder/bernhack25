907142c59f060d5fa27331f90e1bae5942cc7c8b
Shivram Sambhus <shivram.sambhus@gmail.com>
2025-08-23 00:47:44 +0200

    frontend init (sveltekit + shadcn)

---
+++ services/frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="">
+	<ModeWatcher />
+	{@render children?.()}
+</div>
