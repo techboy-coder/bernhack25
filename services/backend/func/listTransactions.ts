6b063960c096327ff1b13d5115836c21b81aae1a
Gian Marco Beltrame <gbeltrame@student.ethz.ch>
2025-08-23 11:14:56 +0200

    experimental projectTransactionsForBankAccount

--- services/backend/func/listTransactions.ts
+++ services/backend/func/listTransactions.ts
@@ -1,5 +1,6 @@
 import { getData } from "../src/getData";
-import type { IncomeCategory, ExpenseCategory, Transaction } from "../src/schema";
+import type { IncomeCategory, ExpenseCategory, Transaction, RecurrentPayment } from "../src/schema";
+import { DateTime } from 'luxon'
 
 type TransactionFilter = {
   timeRange?: {
@@ -37,4 +38,76 @@ export function listTransactionsForBankAccount(accountId: string, filter?: Trans
   if(!transactions) return
   
   return applyFilter(transactions, filter)
+}
+
+function getNextRecurrentPaymentDate(recurrentPayment: RecurrentPayment, after?: number) {
+  const afterDateTime = after ? DateTime.fromSeconds(after / 1000) : DateTime.now()
+
+  let result = DateTime.fromISO(recurrentPayment.startDate)
+  if(recurrentPayment.frequency === 'monthly') {
+    result.set({ month: afterDateTime.month, year: afterDateTime.year })
+
+    if(result < afterDateTime) result.plus({ month: 1 })
+  } else if(recurrentPayment.frequency === 'quarterly') {
+    const quarterOffset = result.month % 3
+    const afterQuarter = Math.floor((afterDateTime.month - quarterOffset + 3) / 3) * 3
+
+    result.set({ month: quarterOffset + afterQuarter, year: afterDateTime.year })
+
+    if(result < afterDateTime) result.plus({ month: 3 })
+  } else if(recurrentPayment.frequency === 'weekly') {
+    result.set({ weekYear: afterDateTime.weekYear, year: afterDateTime.year })
+
+    if(result < afterDateTime) result.plus({ week: 1 })
+  } else if(recurrentPayment.frequency === 'yearly') {
+    result.set({ year: afterDateTime.year })
+
+    if(result < afterDateTime) result.plus({ year: 1 })
+  } else throw 'invalid frequency'
+
+  return result.toUnixInteger() * 1000
+}
+
+export function projectTransactionsForBankAccount(accountId: string, filter: TransactionFilter & { timeRange: {
+  start: number,
+  end: number
+} }) {
+  let account = getData().bankAccounts.find(account => account.id === accountId)
+  if(!account) return
+
+  const transactions = listTransactionsForBankAccount(accountId, filter)
+  if(!transactions) return
+
+  type OutstandingRecurrentPayment = {
+    recurrentPaymentId: string,
+    date: number
+  }
+
+  const recurrentTransactionMap: { [key: string]: Transaction[] } = {}
+  for(const transaction of transactions) {
+    if(transaction.recurrentPaymentId) {
+      if(recurrentTransactionMap[transaction.id] === undefined) recurrentTransactionMap[transaction.id] = []
+
+      recurrentTransactionMap[transaction.id].push(transaction)
+    }
+  }
+
+  const outstandingRecurrentPayments: OutstandingRecurrentPayment[] = []
+  for(const recurrentPayment of account.recurrentPayments) {
+    const alreadyMadeTransactions = recurrentTransactionMap[recurrentPayment.id] ?? []
+
+    let date = getNextRecurrentPaymentDate(recurrentPayment, filter.timeRange.start)
+    while(date < filter.timeRange.end) {
+      const hasTransaction = alreadyMadeTransactions.find(transaction => new Date(transaction.date).getTime() == date) !== undefined
+
+      if(!hasTransaction) outstandingRecurrentPayments.push({
+        recurrentPaymentId: recurrentPayment.id,
+        date: date
+      })
+
+      date = getNextRecurrentPaymentDate(recurrentPayment, date)
+    }
+  }
+
+  return outstandingRecurrentPayments
 }
